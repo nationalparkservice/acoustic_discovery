a649bb4caa9ddbae8aee5723d4b172b9a0828c72
Cameron Summers <csummers@gracenote.com>
2019-01-08 14:14:23 -0800

    add phase two 10 models and feature computation

--- nps_acoustic_discovery/feature.py
+++ nps_acoustic_discovery/feature.py
@@ -1,6 +1,6 @@
 __author__ = "Cameron Summers"
 
-from python_speech_features import mfcc
+from python_speech_features import mfcc, fbank
 import numpy as np
 
 
@@ -23,34 +23,50 @@ class FeatureExtractor(object):
         Returns:
             ndarray: feature matrix (time x features)
         """
-        mfcc_feat = mfcc(audio_data, sample_rate,
-                         winstep=self.fconfig['hop_size'],
-                         numcep=self.fconfig['num_cepstral_coeffs'],
-                         nfilt=self.fconfig['num_filters'],
-                         nfft=self.fconfig['nfft'],
-                         lowfreq=self.fconfig['low_freq'],
-                         highfreq=self.fconfig['high_freq'])
+        if self.fconfig['num_cepstral_coeffs'] is not None:
+            # For models that use only mfccs
 
-        # deltas
-        N = 2
-        mfcc_delta = np.zeros(shape=mfcc_feat.shape)
-        for t in range(mfcc_feat.shape[0]):
-            try:
-                numer = np.sum([n * (mfcc_feat[t + n, :] - mfcc_feat[t - n, :]) for n in range(1, N + 1)])
-                denom = 2 * np.sum([n ** 2 for n in range(N)])
-                mfcc_delta[t, :] = np.divide(numer, denom)
-            except IndexError:
-                mfcc_delta[t, :] = mfcc_feat[t, :]
+            mfcc_feat = mfcc(audio_data, sample_rate,
+                             winstep=self.fconfig['hop_size'],
+                             numcep=self.fconfig['num_cepstral_coeffs'],
+                             nfilt=self.fconfig['num_filters'],
+                             nfft=self.fconfig['nfft'],
+                             lowfreq=self.fconfig['low_freq'],
+                             highfreq=self.fconfig['high_freq'])
 
-        # delta deltas
-        mfcc_delta_delta = np.zeros(shape=mfcc_delta.shape)
-        for t in range(mfcc_delta.shape[0]):
-            try:
-                numer = np.sum([n * (mfcc_delta[t + n, :] - mfcc_delta[t - n, :]) for n in range(1, N + 1)])
-                denom = 2 * np.sum([n ** 2 for n in range(N)])
-                mfcc_delta_delta[t, :] = np.divide(numer, denom)
-            except IndexError:
-                mfcc_delta_delta[t, :] = mfcc_delta[t, :]
+            # deltas
+            N = 2
+            mfcc_delta = np.zeros(shape=mfcc_feat.shape)
+            for t in range(mfcc_feat.shape[0]):
+                try:
+                    numer = np.sum([n * (mfcc_feat[t + n, :] - mfcc_feat[t - n, :]) for n in range(1, N + 1)])
+                    denom = 2 * np.sum([n ** 2 for n in range(N)])
+                    mfcc_delta[t, :] = np.divide(numer, denom)
+                except IndexError:
+                    mfcc_delta[t, :] = mfcc_feat[t, :]
 
-        return np.hstack((mfcc_feat, mfcc_delta, mfcc_delta_delta))
+            # delta deltas
+            mfcc_delta_delta = np.zeros(shape=mfcc_delta.shape)
+            for t in range(mfcc_delta.shape[0]):
+                try:
+                    numer = np.sum([n * (mfcc_delta[t + n, :] - mfcc_delta[t - n, :]) for n in range(1, N + 1)])
+                    denom = 2 * np.sum([n ** 2 for n in range(N)])
+                    mfcc_delta_delta[t, :] = np.divide(numer, denom)
+                except IndexError:
+                    mfcc_delta_delta[t, :] = mfcc_delta[t, :]
 
+            feats = np.hstack((mfcc_feat, mfcc_delta, mfcc_delta_delta))
+
+        else:
+            # For models that use only mel bands
+
+            mel_feat, energy = fbank(audio_data, sample_rate,
+                                     winstep=self.fconfig['hop_size'],
+                                     nfilt=self.fconfig['num_filters'],
+                                     nfft=self.fconfig['nfft'],
+                                     lowfreq=self.fconfig['low_freq'],
+                                     highfreq=self.fconfig['high_freq'],
+                                     winfunc=lambda x: np.hamming(x))
+            feats = np.log(mel_feat)
+
+        return feats
